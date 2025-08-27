41bddc08e299664a712edfa992b57b8f83aeeca6
yormanau <aularrivera@gmail.com>
2025-08-27 10:12:29 -0500

    Configuracion de los vinculos y javascript

--- js/menu.js
+++ js/menu.js
@@ -2,6 +2,7 @@
     const openButton = document.querySelector('.nav__menu');
     const menu = document.querySelector('.nav__link');
     const closeMenu = document.querySelector('.nav__close');
+    const navLinks = document.querySelectorAll('.nav__links');
 
     openButton.addEventListener('click', ()=>{
         menu.classList.add('nav__link--show');
@@ -10,8 +11,9 @@
     closeMenu.addEventListener('click', ()=>{
         menu.classList.remove('nav__link--show');
     });
-
-    
-
-
+    navLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            menu.classList.remove('nav__link--show');
+        });
+    });
 })();
